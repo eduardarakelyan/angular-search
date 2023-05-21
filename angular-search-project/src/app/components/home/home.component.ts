7198233851776affeacd9653690b799266365311
Eduard Arakelyan <48018922+eduardarakelyan@users.noreply.github.com>
2023-05-21 13:40:53 -0700

    added services for http calls

--- angular-search-project/src/app/components/home/home.component.ts
+++ angular-search-project/src/app/components/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MyDataService } from 'src/app/services/my-data.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  myData: any;
+  constructor(private myDataService: MyDataService) {}
+
+  ngOnInit() {
+    this.myDataService.getData().subscribe((res:any) => {
+      this.myData = res;
+    })
+  }
 
 }
